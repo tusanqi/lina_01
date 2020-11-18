0a1e311b2c95495191bc053117202666f5437a86
lina <1569241545@qq.com>
2020-11-18 19:20:14 +0800

    添加注释

--- src/main/java/com/itheima/pojo/User.java
+++ src/main/java/com/itheima/pojo/User.java
@@ -4,8 +4,8 @@ package com.itheima.pojo;
  * @date 2020/11/18 17:52
  */
 public class User {
-    private String username;
-    private String password;
+    private String username;//用户名
+    private String password;//密码
     private int age;
     private String address;
 
